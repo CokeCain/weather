e4e6a9af8d88c911ec7baa410805f8e3d04a6bf1
CokeCain <cbabic@pfst.hr>
2021-03-16 13:35:14 +0100

    Bug fixes in Views

--- js/views/dailyView.js
+++ js/views/dailyView.js
@@ -1,4 +1,4 @@
-import View from './View.js';
+import View from './view.js';
 import * as config from '../config.js';
 import * as helpers from '../helpers.js';
 
--- js/views/favoritesVIew.js
+++ js/views/favoritesVIew.js
@@ -1,4 +1,4 @@
-import View from './View.js';
+import View from './view.js';
 import * as config from '../config.js';
 
 class FavoritesView extends View {
